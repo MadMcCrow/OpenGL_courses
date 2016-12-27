b03588db4feda0d27240a7a649b228d465f796c9
MadMacCrow <Unreal51>
2016-12-27 17:18:41 +0100

    improved display functions

--- Sokoban/display.c
+++ Sokoban/display.c
@@ -1,4 +1,3 @@
-
 // GL : glDrawArrays
 // GLEW : presque tout le reste
 
@@ -15,15 +14,21 @@
 
 typedef struct
 {
+    vec3 loc;
+    vec3 look_at;
+    vec3 up;
+}Camera;
+
+typedef struct
+{
     GLFWwindow* window;
     GLuint program_id;
     GLuint vertex_buffer;
     int num_points;
     level_t level;
+    int h, w;
     // WARNING CODE UNVALIDATED BY ARSENE
-    vec3 loc;
-    vec3 look_at;
-    vec3 up;
+    Camera cam;
     mat4 model;         // matrice de transformation
     mat4 modelviewproj;
     GLuint matrix_id;   // connexion avec le vertex shader (point de vue)
@@ -47,17 +52,40 @@ void tile_corner(float* p, float* c, float x, float y, int corner, bool target) 
     // Morton Code (Z)
     int i = corner & 1;
     int j = corner >> 1;
-    p[0] = 0;//x + i;
-    p[1] = 0;//-y - j;
+    p[0] =  x + i;
+    p[1] = -y - j;
     p[2] = 0;
-    c[0] = target ? 1.0f : 0.5f;
-    c[1] = target ? 0.0f : 0.5f;
-    c[2] = target ? 0.0f : 0.5f;
+    c[0] = target ? 0.5f : 1.0f;
+    c[1] = target ? 0.5f : 0.5f;
+    c[2] = target ? 0.5f : 0.5f;
 }
+#if 0
+void box_face_gen(float* p, float* c, const float* pos) {
+// lets make an entire cube of the same color
+            box_face_corner_gen(p +  0 * 2, c +  0 * 2, x, y, 0, target);
+            box_face_corner_gen(p +  3 * 2, c +  3 * 2, x, y, 2, target);
+            box_face_corner_gen(p +  6 * 2, c +  6 * 2, x, y, 1, target);
+            box_face_corner_gen(p +  9 * 2, c +  9 * 2, x, y, 1, target);
+            box_face_corner_gen(p + 12 * 2, c + 12 * 2, x, y, 2, target);
+            box_face_corner_gen(p + 15 * 2, c + 15 * 2, x, y, 3, target);
+}
+
+void box_face_corner_gen(float* p, float* c, float x, float y, float z, int corner, vec3 color) {
+    // Morton Code (Z)
+    int i = corner & 1;
+    int j = corner >> 1;
+    p[0] =  x + i;
+    p[1] = -y - j;
+    p[2] = 0;
+    c[0] = color.r;
+    c[1] = color.g;
+    c[2] = color.b;
+}
+#endif
 
 void box_face(float* p, float* c, const float* q, int face) {
     int s = face & 1;
-    int u = face / 3;
+    int u = face / 2;
     int v = (u + 1) % 3;
     int w = (u + 2) % 3;
     float ku = q[u] + (u == 1 ? -s : s);
@@ -66,32 +94,32 @@ void box_face(float* p, float* c, const float* q, int face) {
     int kv = v == 1 ? -1 : 1;
     int kw = w == 1 ? -1 : 1;
 
-    p[u +  0] = ku;
-    p[v +  0] = q[v];
-    p[w +  0] = q[w];
+    p[u +  0 * 2] = ku;
+    p[v +  0 * 2] = q[v];
+    p[w +  0 * 2] = q[w];
 
-    p[u +  3] = ku;
-    p[v +  3] = q[v];
-    p[w +  3] = q[w] + kw;
+    p[u +  3 * 2] = ku;
+    p[v +  3 * 2] = q[v];
+    p[w +  3 * 2] = q[w] + kw;
 
-    p[u +  6] = ku;
-    p[v +  6] = q[v] + kv;
-    p[w +  6] = q[w];
+    p[u +  6 * 2] = ku;
+    p[v +  6 * 2] = q[v] + kv;
+    p[w +  6 * 2] = q[w];
 
-    p[u +  9] = ku;
-    p[v +  9] = q[v] + kv;
-    p[w +  9] = q[w];
+    p[u +  9 * 2] = ku;
+    p[v +  9 * 2] = q[v] + kv;
+    p[w +  9 * 2] = q[w];
     
-    p[u + 12] = ku;
-    p[v + 12] = q[v];
-    p[w + 12] = q[w] + kw;
+    p[u + 12 * 2] = ku;
+    p[v + 12 * 2] = q[v];
+    p[w + 12 * 2] = q[w] + kw;
 
-    p[u + 15] = ku;
-    p[v + 15] = q[v] + kv;
-    p[w + 15] = q[w] + kw;
+    p[u + 15 * 2] = ku;
+    p[v + 15 * 2] = q[v] + kv;
+    p[w + 15 * 2] = q[w] + kw;
 
-    for(int i = 0; i <= 15; i+=3){
-        c[i + 0] = 0.0;     //R
+    for(int i = 0; i <= 15*2; i+=6){
+        c[i + 0] = 0.0;   //R
         c[i + 1] = 0.3;   //G
         c[i + 2] = 0.8;   //B
     }
@@ -109,36 +137,38 @@ int fill_map_buffer(GLuint vert_buf, const level_t* level) {
     //sossol
     for (int y = 0; y < LVL_HEIGHT; y++) {
         for (int x = 0; x < LVL_WIDTH; x++) {
-            float* p = points + 6 * 3 * (x + (LVL_WIDTH) * y);
-            float* c = colors + 6 * 3 * (x + (LVL_WIDTH) * y);
+            float* p = points + 2 * 6 * 3 * (x + (LVL_WIDTH) * y);
+            float* c = colors + 2 * 6 * 3 * (x + (LVL_WIDTH) * y);
             bool target = level->cells[x + LVL_WIDTH * y].target;
-            tile_corner(p +  0, c +  0, x, y, 0, target);
-            tile_corner(p +  3, c +  3, x, y, 2, target);
-            tile_corner(p +  6, c +  6, x, y, 1, target);
-            tile_corner(p +  9, c +  9, x, y, 1, target);
-            tile_corner(p + 12, c + 12, x, y, 2, target);
-            tile_corner(p + 15, c + 15, x, y, 3, target);
+            tile_corner(p +  0 * 2, c +  0 * 2, x, y, 0, target);
+            tile_corner(p +  3 * 2, c +  3 * 2, x, y, 2, target);
+            tile_corner(p +  6 * 2, c +  6 * 2, x, y, 1, target);
+            tile_corner(p +  9 * 2, c +  9 * 2, x, y, 1, target);
+            tile_corner(p + 12 * 2, c + 12 * 2, x, y, 2, target);
+            tile_corner(p + 15 * 2, c + 15 * 2, x, y, 3, target);
         }
     }
 
-    points += LVL_HEIGHT * LVL_WIDTH * 6 * 3;
-    colors += LVL_HEIGHT * LVL_WIDTH * 6 * 3;
+    points += LVL_HEIGHT * LVL_WIDTH * 6 * 3 * 2;
+    colors += LVL_HEIGHT * LVL_WIDTH * 6 * 3 * 2;
     
     //boiboites
+    #if 1
     for (int y = 0; y < LVL_HEIGHT; y++) {
         for (int x = 0; x < LVL_WIDTH; x++) {
             if (level->cells[x + LVL_WIDTH * y].wall) {
-                float p[] = {x, y, 0};
-                box_face(points + 0 * 6 * 3, colors + 0 * 6 * 3, p, 0);
-                box_face(points + 1 * 6 * 3, colors + 1 * 6 * 3, p, 1);
-                box_face(points + 2 * 6 * 3, colors + 2 * 6 * 3, p, 2);
-                box_face(points + 3 * 6 * 3, colors + 3 * 6 * 3, p, 3);
-                box_face(points + 4 * 6 * 3, colors + 4 * 6 * 3, p, 5);
-                points += 5 * 6 * 3;
-                colors += 5 * 6 * 3;
+                const float p[] = {x, -y, 0};
+                box_face(points + 0 * 6 * 3 * 2, colors + 0 * 6 * 3 * 2, p, 0);
+                box_face(points + 1 * 6 * 3 * 2, colors + 1 * 6 * 3 * 2, p, 1);
+                box_face(points + 2 * 6 * 3 * 2, colors + 2 * 6 * 3 * 2, p, 2);
+                box_face(points + 3 * 6 * 3 * 2, colors + 3 * 6 * 3 * 2, p, 3);
+                box_face(points + 4 * 6 * 3 * 2, colors + 4 * 6 * 3 * 2, p, 5);
+                points += 5 * 6 * 3 * 2;
+                colors += 5 * 6 * 3 * 2;
             }
         }
     }
+    #endif
 
     glUnmapBuffer(GL_ARRAY_BUFFER);
     return num_points;
@@ -151,11 +181,8 @@ void display(GLFWwindow* window, const Application* app)
        
     // utilisation des shaders identifiés
     glUseProgram(app->program_id);
-    // active l'accès à l'attribut 0 de la source courante
-     
-    // WARNING USE OF CODE UNVERIFIED BY ARSENE
+
     glUniformMatrix4fv(app->matrix_id, 1, GL_FALSE, &app->modelviewproj.matrix[0][0] );
-    // END OF WARNING
      
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
@@ -166,7 +193,7 @@ void display(GLFWwindow* window, const Application* app)
         3,                          // taille
         GL_FLOAT,                   // type
         GL_FALSE,                   // normalisé
-        sizeof(float) * 3,          // vertex size
+        sizeof(float) * 6,          // vertex size
         (void*)0                    // décalage
     );
     glVertexAttribPointer(
@@ -174,7 +201,7 @@ void display(GLFWwindow* window, const Application* app)
         3,                          // taille
         GL_FLOAT,                   // type
         GL_FALSE,                   // normalisé
-        sizeof(float) * 3,          // vertex size
+        sizeof(float) * 6,          // vertex size
         (void*)(sizeof(float) * 3)  // décalage
     );
     glDrawArrays(GL_TRIANGLES, 0, app->num_points);
@@ -188,17 +215,18 @@ void display(GLFWwindow* window, const Application* app)
 // ------------------------- unstable work here ----------------------
 
 void set_mvp(Application* app) {
-    mat4 projection_matrix = mat4_perspective( 3.14159/2, 800. / 800., .1, 100. );
-    mat4 view_matrix       = mat4_lookAt(app->loc, app->look_at, app->up);
+    
+    mat4 projection_matrix = mat4_perspective( 3.14159/2, (float)app->w / (float)app->h, .1, 100. );
+    mat4 view_matrix       = mat4_lookAt(app->cam.loc, app->cam.look_at, app->cam.up);
     app->modelviewproj     = mat4_product(mat4_product(projection_matrix, view_matrix), app->model);
 }
 
 void init_mvp(Application* app){
     app->model     = mat4_identity();
     app->matrix_id = glGetUniformLocation( app->program_id, "MVP" );
-    app->loc       = vec3_init( 12.5, 0, 10 );
-    app->look_at   = vec3_init( 12.5, -12.5, 0 );
-    app->up        = vec3_init( 0, 0, 1 );
+    app->cam.loc       = vec3_init( 2.5, -2.5, 0.5 );
+    app->cam.look_at   = vec3_init( 12.5, -12.5, 0 );
+    app->cam.up        = vec3_init( 0, 0, 1 );
     set_mvp(app);
 }
 
@@ -214,6 +242,9 @@ int main(int argc, char** argv)
     
     // 1) Connexion au système graphique natif & initialisation
     if (!glfwInit()) return 1;
+    
+
+
 
     // 2) creation d'une fenetre
     // version 3.3
@@ -221,8 +252,12 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    Application app;
+    app.h = 600;
+    app.w = 800;
 
-    GLFWwindow* window = glfwCreateWindow( 800, 800, "SUPER SOKOBAN 2016 version", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow( app.w, app.h, "SUPER SOKOBAN 2016 version", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return 1;
@@ -232,13 +267,12 @@ int main(int argc, char** argv)
     glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
     glewExperimental = 1;
     if (glewInit() != GLEW_OK) {
         glfwTerminate();
         return 1;
     }
-
-    Application app;
 
     static char vertex_shader[] =
         "#version 330 core\n"
@@ -248,7 +282,7 @@ int main(int argc, char** argv)
         "out vec3 fragmentColor; \n"
         "void main() \n"
         "{ \n"
-        "  gl_Position = MVP * vec4(vertexPosition_modelspace,1); \n"
+        "  gl_Position = MVP * vec4(vertexPosition_modelspace,1) ; \n"
         "  fragmentColor = vertexColor; \n"
         "} \n";
     char fragment_shader[] =
@@ -271,39 +305,45 @@ int main(int argc, char** argv)
         fprintf(stderr, "cannot load level\n");
         return 1;
     }
-    
-    // WARNING
-    
-    // END OF WARNING
-
     GLuint vao_id;
-    glGenVertexArrays(1, &vao_id);
+    glGenVertexArrays(2, &vao_id);
     glBindVertexArray(vao_id);
-    
-    #if 0
-    GLfloat vertices[] =
-    {
-       -1.0, -1.0, 0.0,
-        1.0, -1.0, 0.0,
-        0.0,  1.0, 0.0,
-    };
-    #endif
     
     glGenBuffers(1, &app.vertex_buffer);
     app.num_points = fill_map_buffer(app.vertex_buffer, &app.level);
-    
+     
     glDisable(GL_CULL_FACE);
+    //glEnable(GL_DEPTH_TEST);
+    //glDepthFunc(GL_LESS);
+    
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     init_mvp(&app);
-
+    
     glfwSetWindowUserPointer(window, &app);
+    
     while (!glfwWindowShouldClose(window))
     {
         //make the map rotate endlessly ;)
-        app.loc = vec3_transform(mat4_rotate(.1, app.up), app.loc);
+        //app.cam.loc     = vec3_transform(mat4_rotate(.1, app.cam.up), app.cam.loc);
+        app.cam.look_at = vec3_transform(mat4_rotate(.001, app.cam.up), app.cam.look_at);
         set_mvp(&app);
         //
         display(window, &app);
-        glfwWaitEvents();
+
+
+        //inputs processing :        
+        
+        //wait for inputs
+        //glfwWaitEvents();
+        
+        //wait for inputs for a certain amount of time (s):
+        //glfwWaitEventsTimeout(1./25.);
+        
+        // see if any inputs have been done:
+        glfwPollEvents();
+        
+
+    
     }
 
     glfwTerminate();
